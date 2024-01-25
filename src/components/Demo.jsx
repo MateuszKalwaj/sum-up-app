20e1c9b2ef67a9b339b09b2dc8e6678527874ef1
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2024-01-25 12:30:44 +0100

    added submit handler to form

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,22 +1,29 @@
 import {copy, linkIcon, loader, tick} from '../assets';
+import {useState} from "react";
 
 const Demo = () => {
 
+    const [article, setArticle] = useState({
+        url: '',
+        summary: ''
+    })
+
+    const handleSubmit = async (e) => {
+        alert('Submitted!')
+    }
 
     return (
         <section className="mt-16 w-full max-w-xl">
             <div className="flex flex-col w-full gap-2">
                 <form className="relative flex justify-center items-center"
-                      onSubmit={() => {
-                      }}
+                      onSubmit={handleSubmit}
                 >
                     <img src={linkIcon} alt="link-icon" className="absolute left-2 my-2"/>
                     <input
                         type="url"
                         placeholder="Enter the URL"
-                        value=""
-                        onChange={() => {
-                        }}
+                        value={article.url}
+                        onChange={(e) => setArticle({...article, url: e.target.value})}
                         required
                         className="url_input peer"
                     />
