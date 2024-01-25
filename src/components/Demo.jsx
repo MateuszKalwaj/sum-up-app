2cc3e63bfd82ecc4f8f15b174d2400c9c42fb028
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2024-01-25 12:21:47 +0100

    form input element added

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,36 @@
+import {copy, linkIcon, loader, tick} from '../assets';
+
 const Demo = () => {
+
+
     return (
-        <div>Demo</div>
+        <section className="mt-16 w-full max-w-xl">
+            <div className="flex flex-col w-full gap-2">
+                <form className="relative flex justify-center items-center"
+                      onSubmit={() => {
+                      }}
+                >
+                    <img src={linkIcon} alt="link-icon" className="absolute left-2 my-2"/>
+                    <input
+                        type="url"
+                        placeholder="Enter the URL"
+                        value=""
+                        onChange={() => {
+                        }}
+                        required
+                        className="url_input peer"
+                    />
+
+                    <button
+                        type="submit"
+                        className="submit_btn peer-focus:border-gray-900 peer-focus:text-gray-900"
+                    >
+                        ↵
+                    </button>
+                </form>
+
+            </div>
+        </section>
     )
 }
 export default Demo
