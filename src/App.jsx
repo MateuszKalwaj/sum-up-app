4380d8c5c82cca14ff636ac260df85ae8b79caa0
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2024-01-25 11:03:58 +0100

    added css and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,18 @@
+import './App.css';
+import Hero from "./components/Hero.jsx";
+import Demo from "./components/Demo.jsx";
 const App = () => {
     return (
-        <div>App</div>
+        <main>
+            <div className="main">
+                <div className="gradient"/>
+            </div>
+
+            <div className="app">
+                <Hero/>
+                <Demo/>
+            </div>
+        </main>
     )
 }
 export default App
