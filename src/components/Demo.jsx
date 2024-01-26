2a75f31eb05edbc97f333c765ce44d208e6dc2dd
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2024-01-26 12:47:54 +0100

    searched articles history array implemented

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -18,7 +18,7 @@ const Demo = () => {
         if (articlesFromLocalStorage) {
             setAllArticles(articlesFromLocalStorage);
         }
-    },[]);
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -61,7 +61,24 @@ const Demo = () => {
                         ↵
                     </button>
                 </form>
+                <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+                    {allArticles.map((item, index) => (
+                        <div
+                            key={`link-${index}`}
+                            onClick={() => setArticle(item)}
+                            className="link_card">
+                            <div className="copy_btn">
+                                <img src={copy}
+                                     alt="copy-icon"
+                                     className="w-[40%] h-[40%] object-contain"/>
+                            </div>
+                            <p className="flex-1 font-satoshi text-blue-900 font-medium text-sm truncate">
+                                {item.url}
+                            </p>
+                        </div>
 
+                    ))}
+                </div>
             </div>
         </section>
     )
